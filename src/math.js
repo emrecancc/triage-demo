a07f3cdf4c1e5757e6ade033182fa921622621d1
emrecancc <emrecan.cerli@gmail.com>
2026-06-19 11:41:18 +0300

    feat: add formatted result output using chalk

--- src/math.js
+++ src/math.js
@@ -1,9 +1,15 @@
+const chalk = require("chalk");
+
 function add(a, b) {
   return a + b;
+}
+
+function formatResult(label, value) {
+  return chalk.green(`${label}: ${value}`);
 }
 
 function multiply(a, b) {
   return a * b;
 }
 
-module.exports = { add, multiply };
+module.exports = { add, multiply, formatResult };
