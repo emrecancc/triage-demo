c134c8a0bd32765a01d9253b645c31affc876310
emrecancc <emrecancerli55@gmail.com>
2026-06-22 15:23:16 +0300

    refactor: simplify math module (scenario 2023)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,7 @@
 const chalk = require("chalk");
 
-function add(a, b) {
+function add(a, b => {
   return a + b;
 }
 
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+module.exports = { add };
