6e102f4595512a558604b1fb57792083c530cc48
emrecancc <emrecancerli55@gmail.com>
2026-06-24 19:43:24 +0300

    feat: integrate express+fastify helpers (scenario 2755)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,17 @@
 const chalk = require("chalk");
+const express = require("express");
+const fastify = require("fastify");
 
 function add(a, b) {
   return a + b;
-}
-
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
 }
 
 function multiply(a, b) {
   return a * b;
 }
 
-module.exports = { add, multiply, formatResult };
+function format(label, value) {
+  return chalk.cyan(`${label}=${value}`);
+}
+
+module.exports = { add, multiply, format };
