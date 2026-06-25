1d86a6279afdd7cf4173297c044fdb94cf47413a
emrecancc <emrecancerli55@gmail.com>
2026-06-25 07:32:27 +0300

    test: add fixture import (scenario 3454)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,8 @@
 const chalk = require("chalk");
+const testData = require("./mocks/api-client");
 
 function add(a, b) {
   return a + b;
 }
 
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+module.exports = { add };
