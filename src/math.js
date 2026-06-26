c2d48f8192da944293c2e52b7b6a833c283c856e
emrecancc <emrecancerli55@gmail.com>
2026-06-26 11:16:20 +0300

    feat: integrate pretty-ms (scenario v3-4432)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,8 @@
 const chalk = require("chalk");
+const pretty_ms = require("pretty-ms");
 
-function add(a, b) {
-  return a + b;
-}
+function add(a, b) { return a + b; }
+function multiply(a, b) { return a * b; }
+function format(label, value) { return chalk.green(`${label}: ${value}`); }
 
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+module.exports = { add, multiply, format };
