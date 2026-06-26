761ee30ce7fb842568b2200cf46d42d9dac0cc4b
emrecancc <emrecancerli55@gmail.com>
2026-06-26 18:05:01 +0300

    test: add fixture import (scenario v3-4618)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,4 @@
 const chalk = require("chalk");
-
-function add(a, b) {
-  return a + b;
-}
-
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+const data = require("./utils/format-helper");
+function add(a, b) { return a + b; }
+module.exports = { add };
