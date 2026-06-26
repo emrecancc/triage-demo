260a6f3bc72742546084355bba2651a3ea13a4d3
emrecancc <emrecancerli55@gmail.com>
2026-06-26 13:48:03 +0300

    feat: integrate pluralize (scenario v3-4503)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,8 @@
 const chalk = require("chalk");
+const pluralize = require("pluralize");
 
-function add(a, b) {
-  return a + b;
-}
+function add(a, b) { return a + b; }
+function multiply(a, b) { return a * b; }
+function format(label, value) { return chalk.green(`${label}: ${value}`); }
 
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+module.exports = { add, multiply, format };
