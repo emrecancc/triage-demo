983906900da417af2ddbbb28aa721d489c5d7564
emrecancc <emrecancerli55@gmail.com>
2026-06-26 16:13:26 +0300

    feat: integrate minimatch (scenario v3-4583)

--- src/math.js
+++ src/math.js
@@ -1,15 +1,8 @@
 const chalk = require("chalk");
+const minimatch = require("minimatch");
 
-function add(a, b) {
-  return a + b;
-}
+function add(a, b) { return a + b; }
+function multiply(a, b) { return a * b; }
+function format(label, value) { return chalk.green(`${label}: ${value}`); }
 
-function formatResult(label, value) {
-  return chalk.green(`${label}: ${value}`);
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-module.exports = { add, multiply, formatResult };
+module.exports = { add, multiply, format };
