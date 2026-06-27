443c6626ff8c7eed1ce0d727ab5abeb466876fa2
emrecancc <emrecancerli55@gmail.com>
2026-06-27 15:28:56 +0300

    Revert "feat: integrate tar (scenario v3-4999)"
    This reverts commit 972059ec112993a612ceb3106bd838b8ad405973.

--- src/math.js
+++ src/math.js
@@ -1,8 +1,15 @@
 const chalk = require("chalk");
-const tar = require("tar");
 
-function add(a, b) { return a + b; }
-function multiply(a, b) { return a * b; }
-function format(label, value) { return chalk.green(`${label}: ${value}`); }
+function add(a, b) {
+  return a + b;
+}
 
-module.exports = { add, multiply, format };
+function formatResult(label, value) {
+  return chalk.green(`${label}: ${value}`);
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = { add, multiply, formatResult };
